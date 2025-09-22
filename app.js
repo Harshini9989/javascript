2272be7b54f090cef4265d3120c05975d8922863
harshini <harshinivallapu@gmail.com>
2025-09-22 11:02:29 +0530

    entered datatypes

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-var a=10;
-var a=90;
-let b=68;
-const c=78;
+// var a=10;
+// var a=90;
+// let b=68;
+// const c=78;
 
-console.log('value b is ${b}');
-console.log(a);
+// console.log('value b is ${b}');
+// console.log('value c is ${c}');
+// console.log(a);
+
+
+a=7897;
+console.log(typeof a);
+name=  "jhon";
+console.log(typeof name);
+c = true;
+console.log(typeof c);
+data = null;
+console.log(typeof data);
+d = undefined;
+console.log(typeof d);
+
+array=[56, "jhon", true, null];
+console.log(typeof array);
+obj ={
+    name:"jhon",
+    age :"78",
+    dept:"CSE",
+};
+console.log(typeof data);
